e3587e05e419358afc132f56210a92d27c4df006
sbkbrah <k.stannard@live.com>
2020-01-13 18:38:26 -0600

    Refactor flow layout configuration method into a helper file. Add weak self to getFollower methods capture list to prevent memory leak.

--- GithubFollowers/Screens/FollowerListController.swift
+++ GithubFollowers/Screens/FollowerListController.swift
@@ -10,9 +10,7 @@ import UIKit
 
 class FollowerListController: UIViewController {
     
-    enum Section {
-        case main
-    }
+    enum Section { case main }
 
     var username: String!
     var followers = [Follower]()
@@ -39,7 +37,9 @@ class FollowerListController: UIViewController {
     }
     
     fileprivate func getFollowers() {
-        NetworkManager.shared.getFollowers(from: username, page: 1) { result in
+        NetworkManager.shared.getFollowers(from: username, page: 1) { [weak self] result in
+            guard let self = self else { return }
+            
             switch result {
             case .success(let followers):
                 self.followers = followers
@@ -66,23 +66,9 @@ class FollowerListController: UIViewController {
     }
     
     fileprivate func configureCollectionView() {
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UIHelper.createThreeColumnFlowLayout(in: view))
         view.addSubview(collectionView)
-        collectionView.backgroundColor = .systemPink
+        collectionView.backgroundColor = .systemBackground
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
-    }
-    
-    fileprivate func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout {
-        let width = view.bounds.width
-        let padding: CGFloat = 12
-        let minimumItemSpacing: CGFloat = 10
-        let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
-        let itemWidth = availableWidth / 3
-        
-        let flowLayout = UICollectionViewFlowLayout()
-        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
-        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 35)
-        
-        return flowLayout
     }
 }
