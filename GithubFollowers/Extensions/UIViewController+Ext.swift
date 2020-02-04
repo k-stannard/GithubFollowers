7465591d40fef4568e90f662020f86c897142433
sbkbrah <k.stannard@live.com>
2020-02-03 20:29:22 -0600

    Add loading view for getFollowers network call.

--- GithubFollowers/Extensions/UIViewController+Ext.swift
+++ GithubFollowers/Extensions/UIViewController+Ext.swift
@@ -8,13 +8,45 @@
 
 import UIKit
 
+fileprivate var containerView: UIView!
+
 extension UIViewController {
+    
     func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String) {
         DispatchQueue.main.async {
             let alertView = GFAlertController(title: title, message: message, buttonTitle: buttonTitle)
             alertView.modalPresentationStyle = .overFullScreen
             alertView.modalTransitionStyle = .crossDissolve
             self.present(alertView, animated: true)
+        }
+    }
+    
+    func showLoadingView() {
+        containerView = UIView(frame: view.bounds)
+        view.addSubview(containerView)
+        
+        containerView.backgroundColor = .systemBackground
+        containerView.alpha = 0
+        
+        UIView.animate(withDuration: 0.25) { containerView.alpha = 0.8 }
+        
+        let activityIndicator = UIActivityIndicatorView(style: .large)
+        containerView.addSubview(activityIndicator)
+        
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+        
+        activityIndicator.startAnimating()
+    }
+    
+    func dismissLoadingView() {
+        DispatchQueue.main.async {
+            containerView.removeFromSuperview()
+            containerView = nil
         }
     }
 }
--- GithubFollowers/Screens/FollowerListController.swift
+++ GithubFollowers/Screens/FollowerListController.swift
@@ -39,8 +39,10 @@ class FollowerListController: UIViewController {
     }
     
     fileprivate func getFollowers(username: String, page: Int) {
+        showLoadingView()
         NetworkManager.shared.getFollowers(from: username, page: page) { [weak self] result in
             guard let self = self else { return }
+            self.dismissLoadingView()
             
             switch result {
             case .success(let followers):
